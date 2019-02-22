c471484847f3d32c445cd1b4f631f923f9481955
Ryan P <rpearson94@outlook.com>
2019-02-22 11:41:59 -0500

    Testing ssh key

--- my_bot.js
+++ my_bot.js
@@ -1,13 +1,13 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log("Connected as " + client.user.tag)
-})
+	console.log('Connected as ' + client.user.tag);
+});
 
 // Get your bot's secret token from:
 // https://discordapp.com/developers/applications/
 // Click on your application -> Bot -> Token -> "Click to Reveal Token"
-bot_secret_token = "NTQ4MzQxMDU0MTEwNDMzMjkx.D1D6nw.NW2SFkmUYDMINI1RcGRDFlQSAj0"
+bot_secret_token = 'NTQ4MzQxMDU0MTEwNDMzMjkx.D1D6nw.NW2SFkmUYDMINI1RcGRDFlQSAj0';
 
-client.login(bot_secret_token)
+client.login(bot_secret_token);
