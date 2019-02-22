4f020a55e9a9cbca109ba97e569d05bf3e0c349e
Ryan P <rpearson94@outlook.com>
2019-02-22 11:48:50 -0500

    Added console log to list servers connected to.

--- my_bot.js
+++ my_bot.js
@@ -5,9 +5,14 @@ client.on('ready', () => {
 	console.log('Connected as ' + client.user.tag);
 });
 
-// Get your bot's secret token from:
-// https://discordapp.com/developers/applications/
-// Click on your application -> Bot -> Token -> "Click to Reveal Token"
+client.on('ready', () => {
+	// List servers the bot is connected to
+	console.log('Servers:');
+	client.guilds.forEach((guild) => {
+		console.log(' - ' + guild.name);
+	});
+});
+
 bot_secret_token = 'NTQ4MzQxMDU0MTEwNDMzMjkx.D1D6nw.NW2SFkmUYDMINI1RcGRDFlQSAj0';
 
 client.login(bot_secret_token);
