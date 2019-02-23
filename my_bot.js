b6cee82af3d1be0ffb4089f6fb00147dd9bf1fa3
Ryan P <rpearson94@outlook.com>
2019-02-23 00:07:07 -0500

    Added the hello world function and channel console log info.

--- my_bot.js
+++ my_bot.js
@@ -13,6 +13,24 @@ client.on('ready', () => {
 	});
 });
 
+client.on('ready', () => {
+	// List servers the bot is connected to
+	console.log('Servers:');
+	client.guilds.forEach((guild) => {
+		console.log(' - ' + guild.name);
+
+		// List all channels
+		guild.channels.forEach((channel) => {
+			console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`);
+		});
+	});
+});
+
+client.on('ready', () => {
+	var generalChannel = client.channels.get('548340725759213590'); // Replace with known channel ID
+	generalChannel.send('Hello, world!');
+});
+
 bot_secret_token = 'NTQ4MzQxMDU0MTEwNDMzMjkx.D1D6nw.NW2SFkmUYDMINI1RcGRDFlQSAj0';
 
 client.login(bot_secret_token);
